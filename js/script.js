6feceaf1097270cf27a9c8bde75f3424ea367be8
Mohammed Alshahrani <mmogib@gmail.com>
2019-05-18 13:24:00 +0300

    last of ex 4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-(function() {
-  document.querySelector("p").innerHTML = "This is even better..";
-})();
+const empSection = document.querySelector("#employment > table > tbody");
+const pubSection = document.querySelector("#publications table tbody");
+fetch("../data/data.json")
+  .then(data => data.json())
+  .then(data => {
+    const emp = data.employment;
+    const pub = data.publications;
+    const empHTML = emp
+      .map((v, i) => {
+        return `
+      <tr>
+          <th scope="row">${i + 1}</th>
+          <td>${v.job}</td>
+          <td>${v.from}</td>
+          <td>${v.to}</td>
+      </tr>
+      `;
+      })
+      .join("");
+
+    const pubHTML = pub
+      .map((v, i) => {
+        return `
+      <tr>
+        <th scope="row">${i + 1}</th>
+        <td>${v.title}</td>
+      </tr>
+      `;
+      })
+      .join("");
+    empSection.innerHTML = empHTML;
+    pubSection.innerHTML = pubHTML;
+  });
